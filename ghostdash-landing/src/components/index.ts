cd7355b6aa77c790ff9a608e3228b5284e48f445
BEYOND BV <idontkn0wtheusername@users.noreply.github.com>
2026-03-05 03:13:02 +0100

    Your commit message

--- ghostdash-landing/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { Hero } from './Hero';
-export { Features } from './Features';
-export { Dashboards } from './Dashboards';
-export { HowItWorks } from './HowItWorks';
-export { CTA } from './CTA';
-export { Footer } from './Footer';
